ac240b399ba239b7792d48ec10753367153c3f5b
Klaudiusz Pupiec <klaudiuszpupiec.kontakt@gmail.com>
2021-10-11 23:38:09 +0200

    add function 'generateTitleLIst'

--- js/script.js
+++ js/script.js
@@ -39,3 +39,45 @@ const links = document.querySelectorAll('.titles a');
 for(let link of links){
     link.addEventListener('click', titleClickHandler);
 }
+
+const optArticleSelector = '.post',
+    optTitleSelector = '.post-title',
+    optTitleListSelector = '.titles';
+
+function generateTitleLinks(){
+
+  /* remove contents of titleList */
+    const titleList = document.querySelectorAll(optTitleListSelector);
+    titleList.innerHTML = '';
+
+  /* for each article */
+    const articles = document.querySelectorAll(optTitleListSelector);
+    
+    let html = '';
+
+    for(let article of articles) {
+        article.classList.add('active');
+        //let titleList = document.querySelectorAll(optTitleListSelector);
+        console.log('dziala');
+
+    /* get the article id */
+    const articleId = document.getElementById('article'); 
+    /* find the title element */
+    const articleTitle = document.querySelector(optTitleSelector).innerHTML;
+
+    /* get the title from the title element */
+
+    /* create HTML of the link */
+    const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+    //console.log(linkHTML);
+    /* insert link into titleList */
+    linkHTML.insertAdjecentHTML('beforebegin', titleList.innerHTML);
+
+    html = html + linkHTML;
+    console.log('html');
+    } 
+
+    titleList.innerHTML = html;
+}
+
+generateTitleLinks();
