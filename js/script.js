82eaf453a17b8fd3fdfb413a98765aaf851c1d16
Klaudiusz Pupiec <klaudiuszpupiec.kontakt@gmail.com>
2021-10-13 21:17:32 +0200

    change function 'generateTitleLinks

--- js/script.js
+++ js/script.js
@@ -47,23 +47,21 @@ const optArticleSelector = '.post',
 function generateTitleLinks(){
 
   /* remove contents of titleList */
-  const titleList = document.querySelectorAll(optTitleListSelector);
+  const titleList = document.querySelector(optTitleListSelector);
   titleList.innerHTML = '';
 
   /* for each article */
-  const articles = document.querySelectorAll(optTitleListSelector);
+  const articles = document.querySelectorAll(optArticleSelector);
     
-  let html = '';
 
   for(let article of articles) {
-    article.classList.add('active');
     //let titleList = document.querySelectorAll(optTitleListSelector);
-    console.log('dziala');
+    //console.log('dziala');
 
     /* get the article id */
-    const articleId = document.getElementById('article'); 
+    const articleId = article.getAttribute('id'); 
     /* find the title element */
-    const articleTitle = document.querySelector(optTitleSelector).innerHTML;
+    const articleTitle = article.querySelector(optTitleSelector).innerHTML;
 
     /* get the title from the title element */
 
@@ -71,13 +69,8 @@ function generateTitleLinks(){
     const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
     //console.log(linkHTML);
     /* insert link into titleList */
-    linkHTML.insertAdjecentHTML('beforebegin', titleList.innerHTML);
-
-    html = html + linkHTML;
-    console.log('html');
+    titleList.insertAdjacentHTML('beforebegin', linkHTML);
   } 
-
-  titleList.innerHTML = html;
 }
 
 generateTitleLinks();
